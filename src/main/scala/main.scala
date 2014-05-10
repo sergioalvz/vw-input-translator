27bb9c2a921939746e252075731de3fdb7100765
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2014-05-10 15:57:10 +0200

    Add logic for saving the dictionary created for representing the model.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,26 +1,25 @@
 import java.io._
 import scala.io.Source
+import scala.collection.mutable.ListBuffer
 import java.security.MessageDigest
 
 object Main {
-  def main(args:Array[String]):Unit = {
-    Parser.parse(args, Configuration()) map { config =>
-      val file = config.file
-      val decimals = config.decimals
-      val outputFileName = s"${file}_vw_${decimals}_decimals"
+  def main(args: Array[String]): Unit = {
+    CLIParser.parse(args, Configuration()) map { config =>
+      val translator = new VWTranslator(config)
+      Source.fromFile(config.file).getLines.foreach(line => translator.translate(line))
+      translator.saveDictionary
 
-      if(decimals > -1){
-        Source.fromFile(file).getLines.foreach(line => {
-          val formatted = formatLine(line, decimals)
-          writeLine(formatted, outputFileName)
-        })
-      }
+      println("Number of classes extracted: " + translator.numberOfClasses)
     }
   }
+}
 
-  def writeLine(line:String, file:String):Unit = {
+object Writer {
+  def write(line: String, file: String):Unit = {
     val out = new File(file)
     if(!out.exists) out.createNewFile
+
     val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(out, true)))
     try {
       writer.write(line)
@@ -29,26 +28,61 @@ object Main {
       writer.close()
     }
   }
+}
 
-  def getClass(score:Double):Int = {
-    if(score < 0.0) 0 else 1
+class VWTranslator(config: Configuration) {
+  private[this] val dictionary = new ListBuffer[(String, Int)]()
+
+  def translate(raw: String): Unit = {
+    val columns       = raw.split("\t")
+    val classLabel    = getClass(columns)
+    val formattedLine = s"$classLabel |Tweet ${columns(3)}\n"
+
+    Writer.write(formattedLine, outputFileName)
   }
 
-  def trunkCoordinate(coordinate:String, decimals:Int):String = {
+  def numberOfClasses: Int = dictionary.size
+
+  def saveDictionary: Unit = {
+    val fileName = s"${outputFileName}_dictionary"
+    dictionary.foreach { case (k, c) => Writer.write(s"$c\t$k\n", fileName) }
+  }
+
+  private[this] def outputFileName: String = {
+    var file = config.file
+    if(this.config.inout) s"${file}_vw_inout" else s"${file}_vw_${config.decimals}_decimals"
+  }
+
+  private[this] def getClass(columns: Array[String]): Int =
+    if(config.inout) getBinaryClass(columns(0).toDouble) else getMultiClassLabel(columns(1), columns(2))
+
+  private[this] def getBinaryClass(score: Double): Int = if(score < 0.0) 0 else 1
+
+  private[this] def getMultiClassLabel(lat:String, lng:String): Int = {
+    val trunkedLat = trunkCoordinate(lat)
+    val trunkedLng = trunkCoordinate(lng)
+
+    val key = trunkedLat + "," + trunkedLng
+
+    if(dictionary.isEmpty) {
+      dictionary += key -> 1
+      1
+    } else{
+      val storedClassLabel = getClassLabelInDictionary(key)
+      if(storedClassLabel.isEmpty) {
+        val last = dictionary.last._2
+        dictionary += key -> (last + 1)
+        last + 1
+      }else {
+        storedClassLabel.get._2
+      }
+    }
+  }
+
+  private[this] def getClassLabelInDictionary(key: String): Option[(String, Int)] = dictionary.find(t => t._1 == key)
+
+  private[this] def trunkCoordinate(coordinate: String): String = {
     val parts = coordinate.split("\\.")
-    parts(0) + "." + parts(1).slice(0, decimals)
+    parts(0) + "." + parts(1).slice(0, config.decimals)
   }
-
-  def formatLine(line:String, decimals:Int):String = {
-    val columns = line.split("\t")
-    val lat = trunkCoordinate(columns(1), decimals)
-    val lng = trunkCoordinate(columns(2), decimals)
-
-    val key = lat + ", " + lng
-    val md5 = getMD5(key)
-
-    md5 + " |Tweet " + columns(3) + "\n"
-  }
-
-  def getMD5(key:String):String = MessageDigest.getInstance("MD5").digest(key.getBytes).map("%02x".format(_)).mkString
 }
